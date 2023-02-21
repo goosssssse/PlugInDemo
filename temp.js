e91b711033065f9f5952120cc523c04591e032fe
goose <yzx2020666@163.com>
2023-02-21 19:30:16 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new script
+// @author       Goose
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+})();
