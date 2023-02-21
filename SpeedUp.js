87cbd43d7e2beb07991f9d825330bc05ac7e33a5
goose <yzx2020666@163.com>
2023-02-21 23:06:49 +0800

    1

--- SpeedUp.js
+++ SpeedUp.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  加速bilibili视频
 // @author       Goose
-// @match        *://*.bilibili.com/*
+// @match        *://*.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @license      AGPL-3.0
 // @grant        none
