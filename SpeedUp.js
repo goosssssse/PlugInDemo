ae8fa13ec1804c8125e6f900755b50e3fa382a04
goose <yzx2020666@163.com>
2023-02-22 10:31:50 +0800

    1

--- SpeedUp.js
+++ SpeedUp.js
@@ -13,26 +13,34 @@
 (function() {
     'use strict';
     
-    //视频加速按钮
-    var UpBtnArea = document.createElement("div");//按钮位置
-    UpBtnArea.style.position = "fixed";
-    UpBtnArea.style.zIndex = 10000;
-    UpBtnArea.style.top = "150px";
-    UpBtnArea.style.right = "30px";
-    var UpBtn = document.createElement("button");//按钮
-    UpBtn.textContent = "x15";
-    document.getElementsByTagName("body")[0].appendChild(UpBtnArea);//位置插入
-    UpBtnArea.insertBefore(UpBtn,UpBtnArea.children[0]);//按钮插入
+    // //视频加速按钮
+    // var UpBtnArea = document.createElement("div");//按钮位置
+    // UpBtnArea.style.position = "fixed";
+    // UpBtnArea.style.zIndex = 10000;
+    // UpBtnArea.style.top = "150px";
+    // UpBtnArea.style.right = "30px";
+    // var UpBtn = document.createElement("button");//按钮
+    // UpBtn.textContent = "x15";
+    // document.getElementsByTagName("body")[0].appendChild(UpBtnArea);//位置插入
+    // UpBtnArea.insertBefore(UpBtn,UpBtnArea.children[0]);//按钮插入
     
-    UpBtn.addEventListener("click",speedUp);
+    // UpBtn.addEventListener("click",speedUp);
 
-    function speedUp(){
-      var up = window.setInterval(function(){
-        var video = document.getElementsByTagName("video")[0];
-        if(video){
-          video.playbackRate = 15;
-          window.clearInterval(up);
-        }
-      },1000);
-    }
+    // function speedUp(){
+    //   var up = window.setInterval(function(){
+    //     var video = document.getElementsByTagName("video")[0];
+    //     if(video){
+    //       video.playbackRate = 15;
+    //       window.clearInterval(up);
+    //     }
+    //   },1000);
+    // }
+
+    var tmm = window.setInterval(function(){
+      var video = document.getElementsByTagName("video")[0];
+      if(video){
+        video.playbackRate = 15;
+        window.clearInterval(tmm);
+      }
+    }, 2000);
 })();
