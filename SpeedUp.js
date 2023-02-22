c67a3d34732f0f74a362d770ae21069068285f9c
goose <yzx2020666@163.com>
2023-02-22 10:22:56 +0800

    1

--- SpeedUp.js
+++ SpeedUp.js
@@ -24,15 +24,15 @@
     document.getElementsByTagName("body")[0].appendChild(UpBtnArea);//位置插入
     UpBtnArea.insertBefore(UpBtn,UpBtnArea.children[0]);//按钮插入
     
-    // UpBtn.addEventListener("click",()=>{
-    //   document.querySelector("video").playbackRate = 10;
-    // })
+    UpBtn.addEventListener("click",speedUp);
 
-    UpBtn = window.setInterval(function(){
-      var video = document.getElementsByTagName("video")[0];
-      if(video){
-        video.playbackRate = 10;
-        window.clearInterval(UpBtn);
-      }
-    },1000);
+    function speedUp(){
+      var up = window.setInterval(function(){
+        var video = document.getElementsByTagName("video")[0];
+        if(video){
+          video.playbackRate = 10;
+          window.clearInterval(UpBtn);
+        }
+      },5000);
+    }
 })();
